f9e3198d844c558b53f3a28e00db901c0d030da9
Megan Bailey <mkbailey391@gmail.com>
2018-08-31 16:54:51 -0700

    finished assessment

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log ('loaded');
+let plusBtn = document.getElementById("plus");
+let minusBtn = document.getElementById("minus");
+let input = document.querySelector('input');
+let newValue = document.querySelector("h1");
+let value = "";
+
+
+function incrementValue(value){
+    return value++;
+ }
+
+plusBtn.addEventListener('click', function(evt){
+newValue.innerHTML = value++;
+input.newValue = "";
+})
+incrementValue();
+
+function decrementValue(value){
+    return value--;
+}
+minusBtn.addEventListener('click', function(evt){
+newValue.innerHTML = value--;
+input.newValue = "";
+})
+decrementValue();
